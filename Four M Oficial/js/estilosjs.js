53eca771f98a3a4c86001d8ae7d2154ae944fbd4
FourMpro <fourmproyect@gmail.com>
2022-08-10 19:00:34 -0300

    arreglos

--- Four M Oficial/js/estilosjs.js
+++ Four M Oficial/js/estilosjs.js
@@ -1,11 +1,11 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {functionNavcolapso()};
 
-function scrollFunction() {
+function functionNavcolapso() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navepica").style.padding = "2px 5px";
+    document.getElementById("navbarColapsante").style.padding = "2px 5px";
     document.getElementById("logoBanner").style.width = "250px";
   } else {
-    document.getElementById("navepica").style.padding = "80px 15px";
+    document.getElementById("navbarColapsante").style.padding = "80px 15px";
     document.getElementById("logoBanner").style.width = "550px";
   }
 }
